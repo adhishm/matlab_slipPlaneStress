abb2f47fbfd1db39967d0f2c9122f26dfd60402a
Adhish Majumdar <adhishm@gmail.com>
2013-05-21 17:02:29 +0200

    Modifying function readSlipPlane

---
+++ readSlipPlane.m
@@ -0,0 +1,22 @@
+function slipPlane = readSlipPlane(filename)
+%% slipPlane = readSlipPlane(filename)
+%  Reads the data from the input file whose name is provided in filename.
+
+    %% Initialize variables
+    extremities = zeros(2,3);
+    normalVector = zeros(1,3);
+    position = zeros(1,3);
+
+    %% Read data from the file
+    fp = fopen(filename, 'r');
+    fscanf(fp, 'Dislocation list file: %s\n', dislocationListFile);
+    fscanf(fp, 'Extremities: %f %f %f %f %f %f\n', ...
+        extremities(1,1), extremities(1,2), extremities(1,3), ...
+        extremities(2,1), extremities(2,2), extremities(2,3) );
+    fscanf(fp, 'Normal vector: %f %f %f\n', ...
+        normalVector(1), normalVector(2), normalVector(3) );
+    fscanf(fp, 'Position: %f %f %f', ...
+        position(1), position(2), position(3) );
+    
+    
+end
